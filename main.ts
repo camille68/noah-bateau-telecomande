692b2d10bbb9a3e45a72e35c59cc7f39af6ae53f
camille68 <camille68@gmail.com>
2021-05-02 21:01:48 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 let p_1 = 0
+let p_2 = 0
 basic.forever(function () {
     p_1 = pins.analogReadPin(AnalogPin.P1)
     // mal traduit
     // 
     p_1 = Math.round(Math.map(p_1, 4, 1007, -5, 5))
-    basic.pause(500)
+    basic.pause(200)
+    p_2 = pins.analogReadPin(AnalogPin.P2)
+    // mal traduit
+    // 
+    p_2 = Math.round(Math.map(p_1, 4, 1007, -5, 5))
+    basic.pause(200)
 })
