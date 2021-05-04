0e0a32eec5c9fb7c1e63e88c8156e4966f293661
camille68 <camille68@gmail.com>
2021-05-04 20:44:16 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,12 +11,12 @@ basic.forever(function () {
     p_2 = Math.round(Math.map(p_1, 4, 1007, -5, 5))
     basic.pause(200)
     if (p_1 == 0) {
-    	
+        radio.sendValue("p_1", p_1 * 20)
     } else {
         radio.sendValue("p_1", p_1 * 20)
     }
     if (p_2 == 0) {
-    	
+        radio.sendValue("p_2", p_2 * 20)
     } else {
         radio.sendValue("p_2", p_2 * 20)
     }
